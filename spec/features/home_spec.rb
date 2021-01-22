a66d6cf4a30a945d16742233e3614eb5fe5d280a
Hamish Arro <Hamisharro@icloud.com>
2021-01-22 10:50:59 +0000

    add tests for asking for name and birthday and make them pass

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,8 +1,17 @@
 feature 'Home page' do
 
-  scenario 'it greets the user' do
-    visit('/')
+  background {visit('/')}
+
+  scenario 'greets the user' do
     expect(page).to have_content('Hello there!')
+  end
+
+  scenario 'asks for name' do
+    expect(page).to have_content("What's your name?")
+  end
+
+  scenario 'asks for birthday' do
+    expect(page).to have_content("When's your birthday?")
   end
 
 end
