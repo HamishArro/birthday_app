9bb6d241ecacce8b2cc3f567dbde4823d70de9c0
Hamish Arro <Hamisharro@icloud.com>
2021-01-22 10:33:22 +0000

    configure reloader

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/reloader'
 
 class BirthdayApp < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    "This is the start of my birhtday app"
+  end
 
 end
